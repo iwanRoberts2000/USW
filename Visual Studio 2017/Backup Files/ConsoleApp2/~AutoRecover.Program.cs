04876a4fca9152a527e72cc2be51094a94a19483
iwanRoberts2000 <38667041+iwanRoberts2000@users.noreply.github.com>
2018-11-01 17:22:50 +0000

    Add files via upload

---
+++ Visual Studio 2017/Backup Files/ConsoleApp2/~AutoRecover.Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i < 20; i++)
+            {
+                
+                sum += i;
+                Console.WriteLine(sum);
+
+
+            }
+
+        }
+    }
+
+}
